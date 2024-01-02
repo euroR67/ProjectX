1fa060f3f8af69b699a61dadfa383c685340224d
euroR67 <mchamaev67@gmail.com>
2024-01-02 20:10:54 +0100

    FIX

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use App\Form\MessageType;
 use Symfony\UX\Turbo\TurboBundle;
 use Symfony\Component\Mercure\Update;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,26 +16,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, HubInterface $hub, EntityManagerInterface $entityManager): Response
     {
-        // Création d'un nouveau message
         $message = new Message();
 
         $form = $this->createForm(MessageType::class, $message);
 
-        // Utilisé pour réinitialiser le formulaire après l'envoi "Post"
         $emptyForm = clone $form;
 
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // Enregistrement du message en BDD
-            $entityManager->persist($message);
-            $entityManager->flush();
-            // Force un formulaire vide a initialiser
-            // Il va remplacer le contenu du Turbo Frame après l'envoi "Post"
+                $entityManager->persist($message);
+                $entityManager->flush();
+
             $form = $emptyForm;
+
         }
 
         return $this->render('home/index.html.twig', [
